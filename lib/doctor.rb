152499ad6e3b123ba79b136641fc31457c899358
Natalia Castro <nat.alvz@gmail.com>
2018-06-08 04:14:21 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,8 @@ class Doctor
     @@all
   end
   
-  def new_appointment(date, patient)
-    Appointment.new(self, date, patient)
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
   end
   
   def appointments
