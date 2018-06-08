a80713092e52a37364e4bcf4bee766d455a0650e
Natalia Castro <nat.alvz@gmail.com>
2018-06-08 04:21:17 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -22,6 +22,6 @@ class Patient
   end
     
   def doctors
-    Appointment.all.select {|doctor| doctor.appointment == self}
+    Appointment.all.collect {|appointment| appointment.doctor}
   end
 end 
